62d4eaf1834a5dde9623ef95869d760865e3a4ee
Darren Song <e1525742@u.nus.edu>
2026-08-23 16:25:27 +0800

    Change Duke.java to Nerrad.java and update banner
    This is to personalise the given template to me.

---
+++ src/main/java/Nerrad.java
@@ -0,0 +1,13 @@
+/**
+ * Entry point for the Nerrad chatbot.
+ */
+public class Nerrad {
+    public static void main(String[] args) {
+        String banner = " _   _                         _ \n"
+                + "| \\ | | ___ _ __ _ __ __ _  __| |\n"
+                + "|  \\| |/ _ \\ '__| '__/ _` |/ _` |\n"
+                + "| |\\  |  __/ |  | | | (_| | (_| |\n"
+                + "|_| \\_|\\___|_|  |_|  \\__,_|\\__,_|\n";
+        System.out.println(banner);
+    }
+}
