58fa0566d5e850afc3be09444d271fd89bda1bf2
Darren Song <e1525742@u.nus.edu>
2026-08-23 20:37:57 +0800

    Update to echo commands entered by user, and exit when user types "bye"

--- src/main/java/Nerrad.java
+++ src/main/java/Nerrad.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Nerrad chatbot.
  */
@@ -15,7 +17,21 @@ public class Nerrad {
         System.out.println("\nHello! I'm Nerrad. My actual name is that backwards! xD");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye! Hope to see you again soon!!!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("You: ");
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("\nNerrad: Bye! Hope to see you again soon!!!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println("\n" + "Nerrad: " + input);
+            System.out.println(separator);
+        }
+        scanner.close();
     }
 }
