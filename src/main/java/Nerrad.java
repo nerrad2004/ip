eb3aceca65b2c598da74da17a4c7eeb877acbdb3
Darren Song <e1525742@u.nus.edu>
2026-08-23 20:11:00 +0800

    Update Nerrad.java to simply greet the user and exit

--- src/main/java/Nerrad.java
+++ src/main/java/Nerrad.java
@@ -3,11 +3,19 @@
  */
 public class Nerrad {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = " _   _                         _ \n"
                 + "| \\ | | ___ _ __ _ __ __ _  __| |\n"
                 + "|  \\| |/ _ \\ '__| '__/ _` |/ _` |\n"
                 + "| |\\  |  __/ |  | | | (_| | (_| |\n"
                 + "|_| \\_|\\___|_|  |_|  \\__,_|\\__,_|\n";
-        System.out.println(banner);
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("\nHello! I'm Nerrad. My actual name is that backwards! xD");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye! Hope to see you again soon!!!");
+        System.out.println(separator);
     }
 }
